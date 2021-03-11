3b90cb3b7981b1c2cb9ad97470fc56cfbf833257
hyrdbyrd <IRuss23@yandex.ru>
2021-03-11 04:24:11 +0300

    add debugging

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const axios = require('axios');
 
 const API_HOST = 'https://music.yandex.ru/';
 
+/** Initialize logging */
+const loggingDecorator = (() => {
+    if (!process.env.DEBUG)
+        return (field, func) => (...args) => func(...args);
+
+    const debugLvl = process.env.DEBUG_LEVEL;
+
+    console.log('YandexApi: DEBUG enabled');
+    debugLvl && console.log('YandexApi: DEBUG LEVEL = ' + debugLvl);
+
+    return (field, func) => (...args) => {
+        console.log(`YandexApi.${field} call: ${JSON.stringify(args)}`);
+
+        const result = new Promise(resolve => resolve(func(...args)));
+
+        if (result instanceof Promise && debugLvl == '2')
+            result
+                .then(res => (console.log(`YandexApi.${field} result: ${res}`, res)))
+                .catch(error => console.error(`YandexApi.${field} error: ${error}`));
+
+        return result;
+    }
+})();
+
 exports.Formatter = class {
     static toQueryString = (obj) => `?${Object.entries(obj).map(([key, val]) => `${key}=${val}`).join('&')}`;
 };
@@ -9,16 +33,19 @@ exports.Formatter = class {
 exports.YandexMusicApi = class {
     _instance = axios.create({ baseURL: API_HOST });
 
-    getPlaylist = (owner, kinds, lang = 'ru') =>
+    getPlaylist = loggingDecorator('getPlaylist', (owner, kinds, lang = 'ru') =>
         this._instance.get(`/handlers/playlist.jsx${exports.Formatter.toQueryString({ owner, lang, kinds })}`)
             .then(({ data }) => data.playlist)
-            .catch(console.error);
+            .catch(console.error)
+    );
 
-    getAlbum = (album, lang = 'ru') =>
+    getAlbum = loggingDecorator('getAlbum', (album, lang = 'ru') =>
         this._instance.get(`/handlers/album.jsx${exports.Formatter.toQueryString({ album, lang })}`)
-            .then(({ data }) => data);
+            .then(({ data }) => data)
+    );
 
-    getArtist = (artist, lang) =>
+    getArtist = loggingDecorator('getArtist', (artist, lang) =>
         this._instance.get(`/handlers/artist.jsx${exports.Formatter.toQueryString({ artist, lang })}`)
-            .then(({ data }) => data);
+            .then(({ data }) => data)
+    );
 };
